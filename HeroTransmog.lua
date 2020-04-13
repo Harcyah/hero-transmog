c263da95d4657bb1048f15c39278463471fa978e
Raphaël Bonnet <harcyah@gmail.com>
2020-04-13 18:58:14 +0200

    Try different functions to get if transmog is known

--- HeroTransmog.lua
+++ HeroTransmog.lua
@@ -1,4 +1,27 @@
 
+local function Debug(tooltip, key, value)
+	if (value == nil) then
+		tooltip:AddLine(key .. " : nil", 0.0, 0.5, 0.3)
+	else
+		tooltip:AddLine(key .. " : " ..  tostring(value), 0.0, 0.5, 0.3)
+	end
+end
+
+local function IsAppearanceKnown(appearanceID)
+	local sources = C_TransmogCollection.GetAllAppearanceSources(appearanceID)
+	if sources then
+		for i, sourceID in pairs(sources) do
+			if sourceID and sourceID ~= NO_TRANSMOG_SOURCE_ID then
+				local _, _, _, _, isCollected = C_TransmogCollection.GetAppearanceSourceInfo(sourceID)
+				if isCollected then
+					return true
+				end
+			end
+		end
+	end
+	return false
+end
+
 local function GameTooltip_OnTooltipSetItem(tooltip)
 	local _, link = tooltip:GetItem()
 	if (not link) then
@@ -23,11 +46,13 @@ local function GameTooltip_OnTooltipSetItem(tooltip)
 
 	-- See https://wow.gamepedia.com/API_C_TransmogCollection.GetSourceInfo
 	local sourceInfo = C_TransmogCollection.GetSourceInfo(sourceId)
-	if (sourceInfo.isCollected) then
-		tooltip:AddLine("Transmog: OK", 0.0, 0.5, 0.3)
-	else
-		tooltip:AddLine("Transmog: Unknown", 0.7, 0.1, 0.0)
-	end
+	-- Debug(tooltip, "sourceInfo/isCollected", sourceInfo.isCollected)
+
+	local playerHasTransmog = C_TransmogCollection.PlayerHasTransmog(itemId)
+	-- Debug(tooltip,"playerHasTransmog", playerHasTransmog)
+
+	local isAppearanceKnown = IsAppearanceKnown(appearanceId)
+	-- Debug(tooltip,"isAppearanceKnown", isAppearanceKnown)
 end
 
 GameTooltip:HookScript("OnTooltipSetItem", GameTooltip_OnTooltipSetItem)
